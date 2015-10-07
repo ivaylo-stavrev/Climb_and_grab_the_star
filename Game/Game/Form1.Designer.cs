a2dc92c0d7fb2a904b37a0fc48f79cdeddcf2039
Alex <alex.page9005@yahoo.com>
2015-10-07 16:50:33 +0300

    Finalized?

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -31,50 +31,98 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
             this.screen = new System.Windows.Forms.Panel();
+            this.controls = new System.Windows.Forms.TextBox();
+            this.Star = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.TextBox();
             this.block4 = new System.Windows.Forms.PictureBox();
             this.block3 = new System.Windows.Forms.PictureBox();
             this.block2 = new System.Windows.Forms.PictureBox();
             this.block1 = new System.Windows.Forms.PictureBox();
             this.block0 = new System.Windows.Forms.PictureBox();
             this.bad_guy = new System.Windows.Forms.PictureBox();
-            this.player = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.GameOver = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Star)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.block4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.block3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.block2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.block1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.block0)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bad_guy)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameOver)).BeginInit();
             this.SuspendLayout();
             // 
             // screen
             // 
             this.screen.BackColor = System.Drawing.Color.Transparent;
             this.screen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.screen.Controls.Add(this.controls);
+            this.screen.Controls.Add(this.Star);
+            this.screen.Controls.Add(this.player);
+            this.screen.Controls.Add(this.score);
             this.screen.Controls.Add(this.block4);
             this.screen.Controls.Add(this.block3);
             this.screen.Controls.Add(this.block2);
             this.screen.Controls.Add(this.block1);
             this.screen.Controls.Add(this.block0);
             this.screen.Controls.Add(this.bad_guy);
-            this.screen.Controls.Add(this.player);
-            this.screen.Controls.Add(this.pictureBox2);
+            this.screen.Controls.Add(this.GameOver);
             this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
             this.screen.Location = new System.Drawing.Point(0, 0);
             this.screen.Name = "screen";
-            this.screen.Size = new System.Drawing.Size(1008, 730);
+            this.screen.Size = new System.Drawing.Size(1184, 662);
             this.screen.TabIndex = 0;
+            // 
+            // controls
+            // 
+            this.controls.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.controls.BackColor = System.Drawing.SystemColors.Info;
+            this.controls.Enabled = false;
+            this.controls.Location = new System.Drawing.Point(897, 0);
+            this.controls.Name = "controls";
+            this.controls.Size = new System.Drawing.Size(287, 20);
+            this.controls.TabIndex = 10;
+            this.controls.Text = "Move = left/right  Jump = space  Restart = enter  Exit = esc";
+            // 
+            // Star
+            // 
+            this.Star.Image = ((System.Drawing.Image)(resources.GetObject("Star.Image")));
+            this.Star.Location = new System.Drawing.Point(584, 10);
+            this.Star.Name = "Star";
+            this.Star.Size = new System.Drawing.Size(32, 54);
+            this.Star.TabIndex = 8;
+            this.Star.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(262, 627);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(16, 32);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // score
+            // 
+            this.score.BackColor = System.Drawing.SystemColors.Info;
+            this.score.Enabled = false;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.score.Location = new System.Drawing.Point(0, 0);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(133, 31);
+            this.score.TabIndex = 6;
             // 
             // block4
             // 
             this.block4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.block4.BackColor = System.Drawing.Color.Red;
             this.block4.Image = global::Game.Properties.Resources._4;
-            this.block4.Location = new System.Drawing.Point(500, 205);
+            this.block4.Location = new System.Drawing.Point(588, 150);
             this.block4.Name = "block4";
             this.block4.Size = new System.Drawing.Size(75, 15);
             this.block4.TabIndex = 5;
@@ -85,7 +133,7 @@
             this.block3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.block3.BackColor = System.Drawing.Color.Red;
             this.block3.Image = global::Game.Properties.Resources._3;
-            this.block3.Location = new System.Drawing.Point(750, 310);
+            this.block3.Location = new System.Drawing.Point(838, 250);
             this.block3.Name = "block3";
             this.block3.Size = new System.Drawing.Size(100, 15);
             this.block3.TabIndex = 4;
@@ -96,7 +144,7 @@
             this.block2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.block2.BackColor = System.Drawing.Color.Red;
             this.block2.Image = global::Game.Properties.Resources._2;
-            this.block2.Location = new System.Drawing.Point(150, 415);
+            this.block2.Location = new System.Drawing.Point(238, 350);
             this.block2.Name = "block2";
             this.block2.Size = new System.Drawing.Size(125, 15);
             this.block2.TabIndex = 3;
@@ -107,7 +155,7 @@
             this.block1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.block1.BackColor = System.Drawing.Color.Red;
             this.block1.Image = global::Game.Properties.Resources._1;
-            this.block1.Location = new System.Drawing.Point(600, 520);
+            this.block1.Location = new System.Drawing.Point(688, 450);
             this.block1.Name = "block1";
             this.block1.Size = new System.Drawing.Size(150, 15);
             this.block1.TabIndex = 2;
@@ -117,40 +165,33 @@
             // 
             this.block0.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.block0.BackColor = System.Drawing.Color.Gold;
-            this.block0.Location = new System.Drawing.Point(250, 625);
+            this.block0.Image = global::Game.Properties.Resources._0;
+            this.block0.Location = new System.Drawing.Point(338, 560);
             this.block0.Name = "block0";
             this.block0.Size = new System.Drawing.Size(500, 15);
             this.block0.TabIndex = 1;
+            this.block0.TabStop = false;
             // 
             // bad_guy
             // 
             this.bad_guy.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.bad_guy.BackColor = System.Drawing.Color.Red;
-            this.bad_guy.Location = new System.Drawing.Point(400, 560);
+            this.bad_guy.BackColor = System.Drawing.Color.Transparent;
+            this.bad_guy.Image = ((System.Drawing.Image)(resources.GetObject("bad_guy.Image")));
+            this.bad_guy.Location = new System.Drawing.Point(490, 482);
             this.bad_guy.Name = "bad_guy";
-            this.bad_guy.Size = new System.Drawing.Size(25, 65);
+            this.bad_guy.Size = new System.Drawing.Size(81, 79);
             this.bad_guy.TabIndex = 0;
             this.bad_guy.TabStop = false;
             // 
-            // player
+            // GameOver
             // 
-            this.player.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.player.BackColor = System.Drawing.Color.Chartreuse;
-            this.player.Location = new System.Drawing.Point(357, 702);
-            this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(25, 25);
-            this.player.TabIndex = 0;
-            this.player.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1008, 730);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
+            this.GameOver.Image = ((System.Drawing.Image)(resources.GetObject("GameOver.Image")));
+            this.GameOver.Location = new System.Drawing.Point(0, 0);
+            this.GameOver.Name = "GameOver";
+            this.GameOver.Size = new System.Drawing.Size(1184, 662);
+            this.GameOver.TabIndex = 9;
+            this.GameOver.TabStop = false;
+            this.GameOver.Visible = false;
             // 
             // timer1
             // 
@@ -160,26 +201,28 @@
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(1184, 662);
             this.Controls.Add(this.screen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.screen.ResumeLayout(false);
+            this.screen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Star)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.block4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.block3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.block2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.block1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.block0)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bad_guy)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameOver)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -195,7 +238,10 @@
         private System.Windows.Forms.PictureBox block2;
         private System.Windows.Forms.PictureBox block1;
         private System.Windows.Forms.PictureBox block4;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox score;
+        private System.Windows.Forms.PictureBox Star;
+        private System.Windows.Forms.PictureBox GameOver;
+        private System.Windows.Forms.TextBox controls;
     }
 }
 
--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -17,6 +17,9 @@ namespace Game
         bool jump;
         int G = 20;
         int Force;
+        int platformSpeed1 = 0;
+        int platformSpeed2 = 0;
+        int points = 1;
         bool check0 = true;
         bool check1 = false;
         bool check2 = true;
@@ -24,28 +27,50 @@ namespace Game
         bool check4 = true;
         bool check_guy1 = false;
         bool check_catched = false;
-        
+        bool check_win = false;
+        bool glitch = false;
 
         public Game()
         {
             InitializeComponent();
+            score.Text = "Score = " + points;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (check_catched)
+            //player textures
+            if (left == false && right == false && jump == false) { player.Image = Image.FromFile("stand.png"); } //standing 
+            if (left == true && jump == true) { player.Image = Image.FromFile("jumpleft.png"); } //jumping to the left side
+            if (right == true && jump == true) { player.Image = Image.FromFile("jumpright.png"); } //jumping to the right side
+            if (left == true && jump == false && glitch == false) { player.Image = Image.FromFile("walkleft.gif"); glitch = true; } //moving to the left side
+            if (right == true && jump == false && glitch == false) { player.Image = Image.FromFile("walkright.gif"); glitch = true; } //moving to the right side
+            if (left == false && right == false && jump == true) { player.Image = Image.FromFile("jumpright.png"); } //jumping on one place
+
+            if (check_catched == true) //game over (when in contact with badGuy
             {
-                //this.Dispose(); // FIXME: This was the only way to stop the game if hit the bad_guy 
+                timer1.Stop();
+                GameOver.Visible = true;
             }
 
-            if (right == true) //moving right
+            if (check_win == true)
             {
-                player.Left += 5;
+                player.Top = screen.Height - player.Height;
+                points++;
+                score.Text = "Score = " + points;
+                if (platformSpeed1 < 6) { platformSpeed1++; }
+                if (points > 4 && points < 12) { platformSpeed2++; }
+                check_win = false;
             }
 
-            if (left == true) //moving left
+
+            if (right == true) //player moving right
             {
-                player.Left -= 5;
+                player.Left += 6;
+            }
+
+            if (left == true) //player moving left
+            {
+                player.Left -= 6;
             }
 
             if (jump == true) //falling (if player jumped)
@@ -65,7 +90,7 @@ namespace Game
                 player.Top += 5; // just falling/gravity
             }
 
-            // side (window) collision
+            // player side (window) collision
             if (player.Right > screen.Right) //right wall
             {
                 player.Left = screen.Width - player.Width;
@@ -85,71 +110,66 @@ namespace Game
             physics(block2);
             physics(block3);
             physics(block4);
-            physics(bad_guy);
 
             //block0 movement and wall collision
             if (block0.Right > screen.Right) { block0.Left = screen.Width - block0.Width; check0 = false; }
             if (block0.Left < screen.Left) { block0.Left = screen.Left; check0 = true; }
-            if (check0 == true) { block0.Left += 0; if (player.Bottom == block0.Top) { player.Left += 0; } }
-            else { block0.Left -= 0; if (player.Bottom == block0.Top) { player.Left -= 0; } }
+            if (check0 == true) { block0.Left += 2; if (player.Bottom == block0.Top) { player.Left += 2; bad_guy.Left += 2; } }
+            else { block0.Left -= 2; if (player.Bottom == block0.Top) { player.Left -= 2; bad_guy.Left -= 2; } }
             //block1 movement and wall collision
             if (block1.Right > screen.Right) { block1.Left = screen.Width - block1.Width; check1 = false; }
             if (block1.Left < screen.Left) { block1.Left = screen.Left; check1 = true; }
-            if (check1 == true) { block1.Left += 5; if (player.Bottom == block1.Top) { player.Left += 5; } }
-            else { block1.Left -= 5; if (player.Bottom == block1.Top) { player.Left -= 5; } }
+            if (check1 == true) { block1.Left += (2 + platformSpeed1); if (player.Bottom == block1.Top) { player.Left += (2 + platformSpeed1); } }
+            else { block1.Left -= (2 + platformSpeed1); if (player.Bottom == block1.Top) { player.Left -= (2 + platformSpeed1); } }
             //block2 movement and wall collision
             if (block2.Right > screen.Right) { block2.Left = screen.Width - block2.Width; check2 = false; }
             if (block2.Left < screen.Left) { block2.Left = screen.Left; check2 = true; }
-            if (check2 == true) { block2.Left += 6; if (player.Bottom == block2.Top) { player.Left += 6; } }
-            else { block2.Left -= 6; if (player.Bottom == block2.Top) { player.Left -= 6; } }
+            if (check2 == true) { block2.Left += (3 + platformSpeed2); if (player.Bottom == block2.Top) { player.Left += (3 + platformSpeed2); } }
+            else { block2.Left -= (3 + platformSpeed2); if (player.Bottom == block2.Top) { player.Left -= (3 + platformSpeed2); } }
             //block3 movement and wall collision
             if (block3.Right > screen.Right) { block3.Left = screen.Width - block3.Width; check3 = false; }
             if (block3.Left < screen.Left) { block3.Left = screen.Left; check3 = true; }
-            if (check3 == true) { block3.Left += 8; if (player.Bottom == block3.Top) { player.Left += 8; } }
-            else { block3.Left -= 8; if (player.Bottom == block3.Top) { player.Left -= 8; } }
+            if (check3 == true) { block3.Left += (4 + platformSpeed1); if (player.Bottom == block3.Top) { player.Left += (4 + platformSpeed1); } }
+            else { block3.Left -= (4 + platformSpeed1); if (player.Bottom == block3.Top) { player.Left -= (4 + platformSpeed1); } }
             //block4 movement and wall collision
             if (block4.Right > screen.Right) { block4.Left = screen.Width - block4.Width; check4 = false; }
             if (block4.Left < screen.Left) { block4.Left = screen.Left; check4 = true; }
-            if (check4 == true) { block4.Left += 9; if (player.Bottom == block4.Top) { player.Left += 9; } }
-            else { block4.Left -= 9; if (player.Bottom == block4.Top) { player.Left -= 9; } }
+            if (check4 == true) { block4.Left += (5 + platformSpeed2); if (player.Bottom == block4.Top) { player.Left += (5 + platformSpeed2); } }
+            else { block4.Left -= (5 + platformSpeed2); if (player.Bottom == block4.Top) { player.Left -= (5 + platformSpeed2); } }
             //bad_guy movement and wall collision
-            if (bad_guy.Right > block0.Right) { bad_guy.Left = block0.Right - bad_guy.Width; check_guy1 = false; }
-            if (bad_guy.Left < block0.Left) { bad_guy.Left = block0.Left; check_guy1 = true; }
-            if (check_guy1 == true) { bad_guy.Left += 2; if (player.Bottom == bad_guy.Top) { player.Left += 2; } }
-            else { bad_guy.Left -= 2; if (player.Bottom == bad_guy.Top) { player.Left -= 2; } }
-        }
+            if (bad_guy.Right > block0.Right) { bad_guy.Left = block0.Right - bad_guy.Width; check_guy1 = false; bad_guy.Image = Image.FromFile("bigbadleft.gif"); }
+            if (bad_guy.Left < block0.Left) { bad_guy.Left = block0.Left; check_guy1 = true; bad_guy.Image = Image.FromFile("bigbadright.gif"); }
+            if (check_guy1 == true) { bad_guy.Left += 4; }
+            else { bad_guy.Left -= 4; }
 
-
-
-
-        public void physics(System.Windows.Forms.PictureBox block)
-        {
-            //side collision
-            //if (player.Right > block.Left && player.Left < block.Right - player.Width && player.Bottom < block.Bottom && player.Bottom > block.Top)
-            //{
-            //    right = false;
-            //}
-
-            //if (player.Left < block.Right && player.Right > block.Left + player.Width && player.Bottom < block.Bottom && player.Bottom > block.Top)
-            //{
-            //    left = false;
-            //}
-
-            //side collision bad_guy
-            // FIXME: I didn't understand why the player is passing through the bad_guy and only hits him in the upper half of the figure?!?
+            //collision with bad_guy (game over)
             if (player.Right > bad_guy.Left && player.Left < bad_guy.Right - player.Width && player.Bottom < bad_guy.Bottom && player.Bottom > bad_guy.Top)
             {
-                right = false;
-                check_catched = true; // FIXME: maybe exeption should be added?!?
+                check_catched = true;
             }
 
             if (player.Left < bad_guy.Right && player.Right > bad_guy.Left + player.Width && player.Bottom < bad_guy.Bottom && player.Bottom > bad_guy.Top)
             {
-                left = false;
-                check_catched = true; // FIXME: maybe exeption should be added?!?
+
+                check_catched = true;
+            }
+            //
+
+            //collision with star (win)
+            if (player.Right > Star.Left && player.Left < Star.Right - player.Width && player.Bottom < Star.Bottom && player.Bottom > Star.Top)
+            {
+                check_win = true;
             }
 
+            if (player.Left < Star.Right && player.Right > Star.Left + player.Width && player.Bottom < Star.Bottom && player.Bottom > Star.Top)
+            {
 
+                check_win = true;
+            }
+        }
+
+        public void physics(System.Windows.Forms.PictureBox block)
+        {
             // top collision 
             if (player.Left + player.Width > block.Left && player.Left + player.Width < block.Left + block.Width + player.Width && player.Top + player.Height >= block.Top && player.Top < block.Top)
             {
@@ -164,35 +184,20 @@ namespace Game
                 jump = true;
             }
 
-
             //bottom collision
-
             if (player.Left + player.Width - 1 > block.Left && player.Left + player.Width < block.Left + block.Width + player.Width && player.Top - block.Bottom <= 10 && player.Top - block.Top > -10)
             {
                 Force = -1;
             }
-
         }
-
-      
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Right)
-            {
-                right = true;
-            }
+            if (e.KeyCode == Keys.Right) { right = true; }
 
-            if (e.KeyCode == Keys.Left)
-            {
+            if (e.KeyCode == Keys.Left) { left = true; }
 
-                left = true;
-            }
-
-            if (e.KeyCode == Keys.Escape)
-            {
-                this.Close();
-            }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
 
             if (jump != true)
             {
@@ -202,21 +207,18 @@ namespace Game
                     Force = G;
                 }
             }
+
+            if (e.KeyCode == Keys.Enter) { Application.Restart(); }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Right)
-            {
-                right = false;
-            }
+            if (e.KeyCode == Keys.Right) { right = false; glitch = false; }
 
-            if (e.KeyCode == Keys.Left)
-            {
+            if (e.KeyCode == Keys.Left) { left = false; glitch = false; }
 
-                left = false;
-            }
-
+            if (e.KeyCode == Keys.Space) { glitch = false; }
         }
     }
 }
+
